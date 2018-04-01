15d60ee7ba97663c88a167a50a62ba2699fc0d8f
Shaheen Ghiassy <shaheen.ghiassy@gmail.com>
2018-03-31 17:57:14 -0700

    Using trello npm

--- handler.ts
+++ handler.ts
@@ -1,46 +1,19 @@
 import { APIGatewayEvent, Callback, Context, Handler } from "aws-lambda"
 let rp = require("request-promise")
+let Trello = require('trello')
 
 let CLIENT_KEY = "1b37c7b593ae2d18fd7bc4e4f91c6b11"
 let OAUTH_TOKEN = "d43c620a08f43da2e859ff67b4490b2fbc72921eade05714b9b1de59dfddc58f"
+let trello = new Trello(CLIENT_KEY, OAUTH_TOKEN)
 
-console.log("starting")
+export const hello: Handler = (event: APIGatewayEvent, context: Context, cb: Callback) => {
+  let boards = trello.getBoards('me')
 
-export const hello: Handler = (
-  event: APIGatewayEvent,
-  context: Context,
-  cb: Callback
-) => {
-  console.log('Function Invoked')
-  console.log("event", event)
-  console.log("context", context)
-
-  rp({
-    uri: "https://api.trello.com/1/member/me/boards",
-    qs: { key: CLIENT_KEY, token: OAUTH_TOKEN },
-    headers: { "User-Agent": "Request-Promise" },
-    json: true
+  boards.then((res) => {
+    let info = res.map((board) => {return {name:board.name, id: board.id}})
+    cb(null, info)
   })
-    .then(function(res) {
-      console.log("res", res)
-
-      cb(null, {
-        statusCode: 200,
-        body: JSON.stringify({
-          trelloResponse: res
-        })
-      })
-    })
-    .catch(function(err) {
-      console.log("err", err)
-
-      cb(null, {
-        statusCode: 200,
-        body: JSON.stringify({
-          trelloError: err
-        })
-      })
-    })
+  .catch((err) => {
+    cb(null, { statusCode: 200, body: JSON.stringify({trelloError: err })})
+  })
 }
-
-console.log("end")
