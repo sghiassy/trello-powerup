27bf0c99ea43bea0d8d888aecff87edf67701e19
Shaheen Ghiassy <shaheen.ghiassy@gmail.com>
2018-04-01 10:58:38 -0700

    adding papertrail logging

--- handler.ts
+++ handler.ts
@@ -1,22 +1,33 @@
 import { APIGatewayEvent, Callback, Context, Handler } from "aws-lambda"
 let rp = require("request-promise")
 let Trello = require("trello")
+let winston = require("winston")
+require("winston-papertrail").Papertrail // Requiring `winston-papertrail` will expose
+
+let winstonPapertrail = new winston.transports.Papertrail({
+  host: "logs3.papertrailapp.com",
+  port: 36550
+})
+
+winstonPapertrail.on("error", function(err) {
+  // Handle, report, or silently ignore connection errors and failures
+})
+
+var logger = new winston.Logger({
+  transports: [winstonPapertrail]
+})
 
 let CLIENT_KEY = "1b37c7b593ae2d18fd7bc4e4f91c6b11"
 let OAUTH_TOKEN = "d43c620a08f43da2e859ff67b4490b2fbc72921eade05714b9b1de59dfddc58f"
 let client = require("request-promise")
 let trello = new Trello(CLIENT_KEY, OAUTH_TOKEN)
 
-export const hello: Handler = (
-  event: APIGatewayEvent,
-  context: Context,
-  cb: Callback
-) => {
+export const hello: Handler = (event: APIGatewayEvent, context: Context, cb: Callback) => {
   let boardsPromise = trello.getBoards("me")
 
-  console.log("hello function called")
-  console.log("event", event)
-  console.log("context", context)
+  logger.info("hello function called")
+  logger.info("event", event)
+  logger.info("context", context)
 
   boardsPromise
     .then(res => {
@@ -26,9 +37,7 @@ export const hello: Handler = (
         return { boardId: { name: board.name, id: boardId } }
       })
 
-      let batchCardsUrl = res
-        .map(board => `/boards/${board.id}/cards`)
-        .join(",")
+      let batchCardsUrl = res.map(board => `/boards/${board.id}/cards`).join(",")
 
       client({
         uri: "https://api.trello.com/1/batch",
@@ -52,19 +61,25 @@ export const hello: Handler = (
                 let cardId = card.id
                 let cardName = card.name
                 info[card.boardId].cards.push({ cardId: { name: cardName } })
-                // console.log('card', card.id, card.name)
+                //  logger.info('card', card.id, card.name)
               }
             }
           }
 
-          console.log("info", info)
-          cb(null, { statusCode: 200, body: JSON.stringify({info:info}) })
+          logger.info("info", info)
+          cb(null, { statusCode: 200, body: JSON.stringify({ info: info }) })
         })
         .catch(err => {
-          cb(null, { statusCode: 200, body: JSON.stringify({trelloBatchError:err}) })
+          cb(null, {
+            statusCode: 200,
+            body: JSON.stringify({ trelloBatchError: err })
+          })
         })
     })
     .catch(err => {
-      cb(null, { statusCode: 200, body: JSON.stringify({trelloBoardsError:err}) })
+      cb(null, {
+        statusCode: 200,
+        body: JSON.stringify({ trelloBoardsError: err })
+      })
     })
 }
