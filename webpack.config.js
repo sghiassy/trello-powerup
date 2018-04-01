cdf6a2fec13600d76906e036e97c64837c7b714e
Shaheen Ghiassy <shaheen.ghiassy@gmail.com>
2018-04-01 01:25:59 -0700

    Updated versions and webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const slsw = require('serverless-webpack');
+const webpack = require('webpack');
 
 module.exports = {
   entry: slsw.lib.entries,
@@ -13,6 +14,9 @@ module.exports = {
       '.tsx'
     ]
   },
+  plugins: [
+    new webpack.IgnorePlugin(/vertx/), // Temp to ignore warning: Module not found: Error: Can't resolve 'vertx' in '/Users/sghiassy/Dev/trello-powerup/node_modules/es6-promise/dist'
+  ],
   output: {
     libraryTarget: 'commonjs',
     path: path.join(__dirname, '.webpack'),
